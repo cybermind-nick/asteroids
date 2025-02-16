a6870a015a34c297447f41af17cff28a6a2c184d
Nicholas Ifeajika <nickifeajika@gmail.com>
2025-02-16 00:08:10 +0000

    restrict fps to 60

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     print("Pygame running? ", pygame.get_init())
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,6 +18,9 @@ def main():
 
         screen.fill('black')
         pygame.display.flip()
+
+        dt = clock.tick(60) # restrict fps to 60fps and return time in milliseconds
+        dt = dt / 1000 # convert milliseconds to seconds
 
 if __name__ == '__main__':
     main()
