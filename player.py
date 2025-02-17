a8d175182b0317b45fbc41001d197323f087d203
Nicholas Ifeajika <nickifeajika@gmail.com>
2025-02-17 11:33:07 +0000

    add timer for shot cooldown using framerate dt

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import *
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 from shot import Shot
 
 class Player(CircleShape):
@@ -9,6 +9,8 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
 
         self.rotation = 0
+
+        self.timer = 0.0
 
         # in the player class
     def triangle(self):
@@ -27,6 +29,9 @@ class Player(CircleShape):
         self.rotation += (PLAYER_TURN_SPEED * dt)
 
     def update(self, dt):
+        
+        self.timer -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -47,9 +52,13 @@ class Player(CircleShape):
 
     def shoot(self):
 
-        shot_velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-        shot = Shot(self.position.x, self.position.y)
-        shot.velocity = shot_velocity
+        if self.timer <= 0:
+            shot_velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            shot = Shot(self.position.x, self.position.y)
+            shot.velocity = shot_velocity
+
+            self.timer = PLAYER_SHOOT_COOLDOWN
+
         
 
 
